264e01531a9b955b532601e250f212df62468aa0
leon <de.nizard@hotmail.com>
2023-02-12 16:59:01 -0300

    Finalizado funções de validação de inputs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const nameInput = document.querySelector('#name')
 const nameSpan = document.querySelector('span.name')
 
+
+
 function printName(){ 
 
     const nameInputValue = nameInput.value
@@ -42,7 +44,6 @@ const mmSpan = document.querySelector('#mm-span')
 
 function printMm(){
     mmSpan.textContent = mmInput.value
-    console.log(mmInput.value)
 }
 mmInput.addEventListener('input', printMm)
 
@@ -88,18 +89,24 @@ form.addEventListener('submit',(e)=>{
 
    
     const isNameValid = nameInput =>{
-        const re = /[A-z]+?/g
+        const re = /^[a-zA-Z\s]+$/
         return re.test(String(nameInput))
+    }
+    const isNumberCardValid = cardNumberInput =>{
+        const re = /^\d{1,4}\s?\d{1,4}\s?\d{1,4}\s?\d{1,4}$/
+        return re.test(cardNumberInput)
     }   
-    
-
+    const isMonthValid = mmInput =>{
+        const re = /^(0[1-9]|1[0-2])$/
+        return re.test(mmInput)
+    }
+    // testando o campo nome no cartao
     if(nameInput.value === ''){
 
         const errorNameP = document.querySelector('#error-name')
         errorNameP.classList.add('error')
         nameInput.classList.add('shake')
         
-
     }else if(!isNameValid(nameInput.value)){
         const errorNameP = document.querySelector('#error-name')
         errorNameP.classList.add('error')
@@ -107,38 +114,111 @@ form.addEventListener('submit',(e)=>{
         nameInput.classList.add('shake')
     }
     
-   
+    //testando o numero informado do cartão
     if(cardNumberInput.value === ""){
 
         const errorNameP = document.querySelector('#error-number')
         errorNameP.classList.add('error')
         errorNameP.textContent = "Can't be blank"
         cardNumberInput.classList.add('shake')
-        
 
-        
-
-    }else if(!Number(cardNumberInput.value)){
+    }else if(!isNumberCardValid(cardNumberInput.value)){
         const errorNameP = document.querySelector('#error-number')
         errorNameP.classList.add('error')
+        errorNameP.textContent = "Wrong format, numbers only"
         cardNumberInput.classList.add('shake')
     }
 
+    //Testando o mes do cartão
+
+    const erroMmP = document.querySelector('#error-mm')
+
+    if(mmInput.value === ""){
+        erroMmP.classList.add('error')
+        mmInput.classList.add('shake')
+    }else if(!isMonthValid(mmInput.value)){
+        erroMmP.classList.add('error')
+        mmInput.classList.add('shake')
+        erroMmP.textContent = "Wrong format, month only"
+        erroMmP.style.fontSize = '0.7em'
+    }
+
+    //testando o ano do cartao
+    const erroYyP = document.querySelector('#error-yy')
+    if(yyInput.value === ""){
+        erroYyP.classList.add('error')
+        erroYyP.textContent = "Can't be blank"
+        yyInput.classList.add('shake')
+
+    }else if(yyInput.value < 23 || yyInput.value > 28){
+    
+        erroYyP.classList.add('error')
+        erroYyP.textContent = "Invalid year"
+        yyInput.classList.add('shake')
+
+    }else if(!Number(yyInput.value)){
+        erroYyP.classList.add('error')
+        erroYyP.textContent = "Wrong format, year only"
+        yyInput.classList.add('shake')
+        erroYyP.style.fontSize = '0.7em'
+    }
+    
+    //testando cvc
+
+    const erroCvcP = document.querySelector('#error-cvc')
+
+    if(cvcInput.value === ""){
+        erroCvcP.classList.add('error')
+        erroCvcP.textContent = "Can't be blank"
+        cvcInput.classList.add('shake')
+
+    }else if(!Number(cvcInput.value)){
+        erroCvcP.classList.add('error')
+        erroCvcP.textContent = "Wrong format, number only"
+        cvcInput.classList.add('shake')
+    }
+    
+    //resetando os erros na tela
+    setTimeout(()=>{
+        nameInput.classList.remove('shake')
+        cardNumberInput.classList.remove('shake')
+        mmInput.classList.remove('shake')
+        yyInput.classList.remove('shake')
+        cvcInput.classList.remove('shake')
+        for (const p of errorP) {
+            p.classList.remove('error')
+        }
+    },2000)
+
+    if(!nameInput.classList.contains('shake') && 
+    !cardNumberInput.classList.contains('shake') && 
+    !mmInput.classList.contains('shake') && 
+    !yyInput.classList.contains('shake') && 
+    !cvcInput.classList.contains('shake')){
+        
+        
+        form.classList.add('none')
+        sucessDiv.classList.add('block')
+    }
 
 })
 
 
-// button.addEventListener('click',()=>{
-//     form.classList.add('none')
 
-//     sucessDiv.classList.add('block')
+continueBtn.addEventListener('click',()=>{
+    form.classList.remove('none')
 
-// })
+    sucessDiv.classList.remove('block')
 
+    const inputAll = document.querySelectorAll('input')
+    for (const inputs of inputAll) {
+        inputs.value = ""
+    }
 
-// continueBtn.addEventListener('click',()=>{
-//     form.classList.remove('none')
+    nameSpan.textContent = "JANE APPLESEED"
+    numberCardSpan.textContent = "0000 0000 0000 0000"
+    mmSpan.textContent = "00"
+    yySpan.textContent = "00"
+    cvcSpan.textContent = "000"
 
-//     sucessDiv.classList.remove('block')
-
-// })
+})
